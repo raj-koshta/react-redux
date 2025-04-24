d630717bd130ad6b5219bde958ce06cba482d0d6
raj-koshta <rajkoshta.rk1@gmail.com>
2025-04-24 20:10:01 +0530

    creating store

--- src/store.jsx
+++ src/store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from "redux"
+
 const ADD_TASK = "task/add"
 const DELETE_TASK = "task/delete"
 
@@ -7,26 +9,42 @@ const initialState = {
 }
 
 // Step-1 Create reducer for your app
-const taskReducer = (state = initialState, action)=>{
+const taskReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TASK:
             return {
                 ...state,
-                task: [ ...state.task , action.payload]
+                task: [...state.task, action.payload]
             };
-        
+
         case DELETE_TASK:
-            const updatedTask = state.task.filter((curTask, index)=>{
+            const updatedTask = state.task.filter((curTask, index) => {
                 return index != action.payload;
             })
             return {
                 ...state,
-                task: [ ...state.task , updatedTask]
+                task: updatedTask,
             };
-    
+
         default:
             return state;
     }
 }
 
 // Step-2 Create the Redux store using the reducer
+
+const store = createStore(taskReducer);
+console.log(store);
+
+// Step-3 Log the initial state
+console.log("initial state: ", store.getState());
+
+// Step-4 Dispatch an action to add the task
+store.dispatch({ type: ADD_TASK, payload: "By some goods" })
+console.log("updated state: ", store.getState());
+
+store.dispatch({ type: ADD_TASK, payload: "By some fruits" })
+console.log("updated state: ", store.getState());
+
+store.dispatch({ type: DELETE_TASK, payload: 0 })
+console.log("Deleted state: ", store.getState());
